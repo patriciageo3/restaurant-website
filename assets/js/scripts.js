a95e6078c476d07199f7350736c0929989fa4c96
Patricia Georgescu <georgescu.patry@gmail.com>
2018-06-13 23:20:52 +0300

    Hamburger menu added + some minor adjustments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,10 @@
 const MENU_BOX = document.getElementsByClassName("container")[0];
-const LOGO = document.getElementById("logo");
 const MENU = document.getElementsByTagName("nav")[0];
 const BRK_POINTS = document.querySelectorAll(".breakpoint");
 const SCROLL_DISTANCES = [];
 const LINKS = Array.from(document.getElementsByClassName("menu_link"));
-
+const MENU_ICON_OPEN = document.getElementById("open_menu");
+const MENU_ICON_CLOSE = document.getElementById("close_menu");
 
 function debounce(func, delay = 40) {
 	let timer;
@@ -19,19 +19,15 @@ function debounce(func, delay = 40) {
 	};
 }
 
-function fixMenu(e) {
+function fixMenu() {
 	let scrolling = window.pageYOffset || document.documentElement.scrollTop;
-
+	
 	if (scrolling > MENU_BOX.scrollHeight) {
 		if (!MENU_BOX.classList.contains("fixed_menu")) {
 			MENU_BOX.classList.add("fixed_menu");
-			LOGO.classList.add("fixed");
-			MENU.classList.add("fixed");
 		}
 	} else {
-		MENU_BOX.className = "container";
-		LOGO.classList.remove("fixed");
-		MENU.classList.remove("fixed");
+		MENU_BOX.classList.remove("fixed_menu");
 	}
 }
 
@@ -82,7 +78,7 @@ function activateMenuOnScroll() {
 
     cleanUpMenu();
     
-  if (scrollDistance < SCROLL_DISTANCES[0].position - navHeight - margin) {
+  if (scrollDistance < SCROLL_DISTANCES[0].position - navHeight - margin || scrollDistance === 0) {
         //Activate Welcome when scrolled to the top/ clicked on Welcome menu item
         LINKS[0].classList.add("active");
         return;
@@ -128,8 +124,27 @@ function smoothScroll(e) {
     }, 100);
 }
 
+function toggleMenuOnClick() {
+	this.classList.remove("show");
+	
+	if (this === MENU_ICON_OPEN) {
+		// activates when open menu icon is clicked
+		MENU_ICON_CLOSE.classList.add("show");
+		MENU_BOX.classList.add("show_menu");
+		MENU.classList.add("show_menu");
+	} else {
+		// activates when close menu icon is clicked
+		MENU_ICON_OPEN.classList.add("show");
+		MENU_BOX.classList.remove("show_menu");
+		MENU.classList.remove("show_menu");
+	}
+	
+}
+
 window.addEventListener("scroll", debounce(fixMenu));
 window.addEventListener("resize", debounce(getBreakPoints, 500));
 window.addEventListener("scroll", debounce(activateMenuOnScroll));
-LINKS.forEach(MenuLink => MenuLink.addEventListener("click", smoothScroll));
 
+LINKS.forEach(MenuLink => MenuLink.addEventListener("click", smoothScroll));
+MENU_ICON_OPEN.addEventListener("click", toggleMenuOnClick);
+MENU_ICON_CLOSE.addEventListener("click", toggleMenuOnClick);
